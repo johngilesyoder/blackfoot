e6897f527f056d52be7427e7e22b322ae1d3c18e
John Yoder <johngilesyoder@gmail.com>
2015-11-20 17:52:17 -0700

    Comment out unused action

--- wp-content/themes/blackfoot/functions.php
+++ wp-content/themes/blackfoot/functions.php
@@ -366,7 +366,7 @@ Actions + Filters + ShortCodes
 // Add Actions
 add_action('init', 'html5blank_header_scripts'); // Add Custom Scripts to wp_head
 add_action('wp_print_scripts', 'html5blank_conditional_scripts'); // Add Conditional Page Scripts
-add_action('get_header', 'enable_threaded_comments'); // Enable Threaded Comments
+//add_action('get_header', 'enable_threaded_comments'); // Enable Threaded Comments
 add_action('wp_enqueue_scripts', 'html5blank_styles', 15); // Add Theme Stylesheet
 add_action('wp_enqueue_scripts', 'html5blank_conditional_styles'); // Add Conditional Theme Stylesheets
 add_action('init', 'register_html5_menu'); // Add HTML5 Blank Menu
