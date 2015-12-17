a372c2225e5bfa4f4bbd0d90549dc982f37a33b6
John Yoder <johngilesyoder@gmail.com>
2015-12-17 11:55:27 -0700

    optimize images

--- wp-content/themes/blackfoot/includes/home/trust.php
+++ wp-content/themes/blackfoot/includes/home/trust.php
@@ -19,7 +19,7 @@
       <div class="row">
         <div class="col-md-3">
           <div class="family-photo">
-            <img class="photo" src="<?php echo get_template_directory_uri(); ?>/assets/img/family.png">
+            <img class="photo" src="<?php echo get_template_directory_uri(); ?>/assets/img/family.jpg">
             <span class="bro-logo">
               <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-text-white.svg">
             </span>
