a49aae0ab142a3cfafef3ae157fc7e740a06f3f6
John Yoder <johngilesyoder@gmail.com>
2015-12-18 15:24:56 -0700

    Add 'Fly' to shop titles

--- wp-content/themes/blackfoot/includes/home/shop.php
+++ wp-content/themes/blackfoot/includes/home/shop.php
@@ -2,11 +2,11 @@
   <div class="container">
     <div class="row">
       <!-- Section title -->
-      <hgroup class="col-md-7">
-        <h2><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-shop-dark.svg">Blackfoot River Outfitters Shop</h2>
+      <hgroup class="col-md-8">
+        <h2><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-shop-dark.svg">Blackfoot River Outfitters Fly Shop</h2>
         <h3>We own and operate a  full-service fly shop to outfit your adventure and fishing needs.</h3>
       </hgroup>
-      <div class="col-md-5">
+      <div class="col-md-4">
         <!-- Shop button -->
         <div class="shop-btn">
           <a href="/shop/" class="btn btn-book">Shop online now</a>
--- wp-content/themes/blackfoot/includes/shop/shop-banner.php
+++ wp-content/themes/blackfoot/includes/shop/shop-banner.php
@@ -1,5 +1,5 @@
 <div class="shop-banner">
   <div class="container">
-    <h2><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-shop-white.svg">Blackfoot River Outfitters Shop</h2>
+    <h2><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-shop-white.svg">Blackfoot River Outfitters Fly Shop</h2>
   </div>
 </div>
