457fee15505b759bd435c649f2765e6bcfa4c671
John Yoder <johngilesyoder@gmail.com>
2015-12-18 16:02:35 -0700

    Add brick and mortar section to product page

--- wp-content/themes/blackfoot/includes/shop/shop-retail-block.php
+++ wp-content/themes/blackfoot/includes/shop/shop-retail-block.php
@@ -4,7 +4,7 @@
       <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/home-hero-5.jpg">
     </div>
     <div class="col-md-8">
-      <h3><span class="glyphicon glyphicon-map-marker"></span> Come visit us!</h3>
+      <h3><span class="glyphicon glyphicon-map-marker"></span> You can see this at our store!</h3>
       <p>We own and operate two full-service <a href="/contact-us/">fly shops</a> to outfit your adventure and fishing needs. But if you just can't wait until you get here, stock up now from our online store.</p>
     </div>
   </div>
