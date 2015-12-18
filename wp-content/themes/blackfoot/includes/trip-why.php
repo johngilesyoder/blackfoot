290a931e15c3c9976fe16948844e0c9442c4658c
John Yoder <johngilesyoder@gmail.com>
2015-12-18 16:15:57 -0700

    Revise trips why text

--- wp-content/themes/blackfoot/includes/trip-why.php
+++ wp-content/themes/blackfoot/includes/trip-why.php
@@ -3,7 +3,7 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <h3>Why plan my adventure with Blackfoot River Outfitters?</h3>
-      <p>Getting people outdoors is our mission, our goal, and our passion. And it's been that way for over 20 years! When you plan a trip with Blackfoot River Outfitters, you experience unique environments through the eyes of expert local guides. <a href="#">Learn more about the Blackfoot River Outfitters experience</a>.</p>
+      <p>Getting people outdoors is our mission, our goal, and our passion. And it's been that way for over 2​5​ years! When you plan a trip with​ Orvis endorsed​ Blackfoot​ ​River Outfitters, you experience unique environments​ and see fly fishing​ through the eyes of expert local guides​ who understand the trip is about you not them​. Don't take our word for it. Read our satisfied <a href="/about-us/testimonials/">customer testimonials &amp; reviews</a>.</p>
     </div>
   </div>
 </aside>
