6c7d567d1f78359d207b9c2ca1e7f467819666b7
John Yoder <johngilesyoder@gmail.com>
2015-12-18 15:22:57 -0700

    Add most popular tag to trips

--- wp-content/themes/blackfoot/includes/home/trip-block.php
+++ wp-content/themes/blackfoot/includes/home/trip-block.php
@@ -2,6 +2,9 @@
   <article class="home-trip">
     <a href="<?php the_permalink(); ?>" class="trip-block" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>');">
       <h1 class="trip-title"><?php the_title(); ?></h1>
+      <?php if ( has_term( 'most-popular', 'product_tag' ) ) : ?>
+        <span class="most-popular">Most Popular</span>
+      <?php endif; ?>
     </a>
     <p class="trip-summary"><?php the_excerpt(); ?></p>
   </article>
