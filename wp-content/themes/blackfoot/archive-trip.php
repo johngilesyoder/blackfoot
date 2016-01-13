f0891c19f021c0b04fcf8936e77ac48cab305144
John Yoder <johngilesyoder@gmail.com>
2016-01-13 04:07:03 -0700

    Fix archive

--- wp-content/themes/blackfoot/archive-trip.php
+++ wp-content/themes/blackfoot/archive-trip.php
@@ -1,37 +1,30 @@
 <?php get_header(); ?>
 
   <main role="main">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-10 col-md-offset-1">
-            <!-- Page Title -->
-            <h1 class="page-title"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-fly.svg"> <?php the_title(); ?></h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <!-- Page Title -->
+          <h1 class="page-title"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-fly.svg"> <?php the_title(); ?></h1>
+          
+          <div class="page-summary">
 
-            <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-            
-            <div class="page-summary">sdfsdf
-
-              <?php 
-                $post_type = get_post_type_object( get_post_type($post) );
-                echo $post_type->description ;
-              ?>
-
-            </div>
-
-            <?php endwhile; ?>
-
-            <?php endif; ?>
+            <?php 
+              $post_type = get_post_type_object( get_post_type($post) );
+              echo $post_type->description ;
+            ?>
 
           </div>
+
         </div>
       </div>
-      <section class="trips">
-        <div class="container">
+    </div>
+    <section class="trips">
+      <div class="container">
 
-         <?php get_template_part('loop-trip'); ?>
+       <?php get_template_part('loop-trip'); ?>
 
-        </div>
-      </section>
+      </div>
     </section>
 
     <!-- Trip Why? -->
