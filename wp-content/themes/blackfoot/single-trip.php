668a7de7fda7b50475bab8901f43a2ad567eafe2
John Yoder <johngilesyoder@gmail.com>
2016-01-13 06:02:01 -0700

    Add smith river sidebar

--- wp-content/themes/blackfoot/single-trip.php
+++ wp-content/themes/blackfoot/single-trip.php
@@ -61,7 +61,7 @@
 								  <p>
 								    <strong>Floatfish</strong>, <strong>Floatfish/Whitewater</strong>, <strong>Walk/Wade Combos</strong>, <strong>Overnight Trips</strong>, and <strong>Scenic trips</strong> available for many Montana waters.
 								  </p>
-								  <a href="/book-a-trip-with-us/" class="btn btn-book">Inquire or Book now</a>
+								  <a href="/contact-us/" class="btn btn-book">Inquire or Book now</a>
 									<? endif; ?>
 								</div>
 								<div class="badge-orvis">
