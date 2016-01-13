db4af0e2df852246dc075d20c2a63a153b650865
John Yoder <johngilesyoder@gmail.com>
2016-01-12 23:27:50 -0700

    Untrack plugins

--- wp-content/plugins/wpsso-um/lib/config-gettext-extract.php
+++
@@ -1,4 +0,0 @@
-<?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array(s)
-_x( 'WPSSO extension to provide updates for the WordPress Social Sharing Optimization (WPSSO) Pro plugin and its Pro extensions.', 'plugin description', 'wpsso-um' );
-?>
