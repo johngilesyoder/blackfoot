5577d0339cc51170ab4b09ad74c25c01aebcfc8a
John Yoder <johngilesyoder@gmail.com>
2016-01-13 01:32:54 -0700

    New Trips page

--- wp-content/themes/blackfoot/includes/trip-block.php
+++ wp-content/themes/blackfoot/includes/trip-block.php
@@ -1,13 +1,21 @@
-<div class="col-md-4">
-  <div class="trip" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>');">
+<?php $from_price = types_render_field("from-price", array("raw"=>"true")); ?>
+
+<div class="trip">
+  <div class="trip-left">
+    <div class="trip-photo" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>');">
+    <?php if ( has_tag( 'most-popular' ) ) : ?>
+      <span class="most-popular">Most Popular</span>
+    <?php endif; ?>
+    </div>
+  </div>
+  <div class="trip-right">
     <div class="trip-content">
       <h2 class="trip-title"><?php the_title(); ?></h2>
       <p class="trip-summary"><?php the_excerpt(); ?></p>
-      <a href="<?php the_permalink(); ?>" class="btn btn-primary">Select options &amp; book now</a>
+      <span class="price">
+        From <span class="amount">$<?php echo $from_price; ?></span> per day
+      </span>
+      <a href="<?php the_permalink(); ?>" class="btn btn-primary">View details &amp; book now</a>
     </div>
-    <?php woocommerce_template_single_price(); ?>
-    <?php if ( has_term( 'most-popular', 'product_tag' ) ) : ?>
-      <span class="most-popular">Most Popular</span>
-    <?php endif; ?>
   </div>
 </div>
--- wp-content/themes/blackfoot/includes/trip-why.php
+++ wp-content/themes/blackfoot/includes/trip-why.php
@@ -1,8 +1,8 @@
 <aside class="trip-why">
-  <hr>
-  <div class="container-fluid">
+  <div class="container">
+    <hr>
     <div class="row">
-      <div class="col-md-8 col-md-offset-2">
+      <div class="col-md-10 col-md-offset-1">
         <h3>Why plan my adventure with Blackfoot River Outfitters?</h3>
         <p>Getting people outdoors is our mission, our goal, and our passion. And it's been that way for over 2​5​ years! When you plan a trip with​ Orvis endorsed​ Blackfoot​ ​River Outfitters, you experience unique environments​ and see fly fishing​ through the eyes of expert local guides​ who understand the trip is about you not them​. Don't take our word for it. Read our satisfied <a href="/about-us/testimonials/">customer testimonials &amp; reviews</a>.</p>
       </div>
