ffabe955700907b9f41efd9b6ff293d30bc979c7
John Yoder <johngilesyoder@gmail.com>
2016-01-12 23:24:22 -0700

    New booking form page template

--- wp-content/themes/blackfoot/includes/book-now-block.php
+++ wp-content/themes/blackfoot/includes/book-now-block.php
@@ -1,7 +1,7 @@
 <div class="book-now-block">
   <h2>Book a trip!</h2>
   <p>
-    <strong>Floatfish</strong>, <strong>Wading</strong>, <strong>Float/wade combos</strong>, <strong>Overnights</strong>, <strong>Cabin</strong>, and <strong>Scenic trips</strong> available for many Montana waters.
+    <strong>Floatfish</strong>, <strong>Floatfish/Whitewater</strong>, <strong>Walk/Wade Combos</strong>, <strong>Overnight Trips</strong>, and <strong>Scenic trips</strong> available for many Montana waters.
   </p>
-  <a href="/trips/" class="btn btn-book">View rates &amp; book now</a>
+  <a href="/trips/" class="btn btn-book">Inquire or Book now</a>
 </div>
