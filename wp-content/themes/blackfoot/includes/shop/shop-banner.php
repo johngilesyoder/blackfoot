d27adb6b4584bbb1270bd91c3e0457e42a640f0e
John Yoder <johngilesyoder@gmail.com>
2016-01-13 17:43:55 -0700

    add no sales tax message

--- wp-content/themes/blackfoot/includes/shop/shop-banner.php
+++ wp-content/themes/blackfoot/includes/shop/shop-banner.php
@@ -1,5 +1,6 @@
 <div class="shop-banner">
   <div class="container">
     <h2><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-shop-white.svg">Blackfoot River Outfitters Fly Shop</h2>
+    <p><strong>No Sales Tax</strong> and <strong>Free Shipping</strong> on orders over $50</p>
   </div>
 </div>
