75994513ddcbcf5ada9939754452308429f0859b
John Yoder <johngilesyoder@gmail.com>
2016-03-24 19:54:10 -0600

    Add links to boats subnav

--- wp-content/themes/blackfoot/includes/boats/boats-subnav.php
+++ wp-content/themes/blackfoot/includes/boats/boats-subnav.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <h1 class="page-title"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-byob-black.svg">Get the Perfect Boat</h1>
     <ul>
-      <li><a href="/sotar-strike-build-your-boat">Build Your Own Strike Raft</a></li>
+      <li><a href="/sotar-strike-build-your-own-boat">Build Your Own Strike Raft</a></li>
       <li><a href="/get-perfect-boat#other-watercraft">Rafts, Kayaks, Cats</a></li>
       <li><a href="/contact-us" class="link-consultation">Schedule a Consultation</a></li>
     </ul>
