fd515d6924609622b2fe88a8e67aca410a6ee661
John Yoder <johngilesyoder@gmail.com>
2016-08-03 11:25:49 -0600

    Update new woocommerce templates

--- wp-content/themes/blackfoot/woocommerce/single-product/product-image.php
+++ wp-content/themes/blackfoot/woocommerce/single-product/product-image.php
@@ -2,16 +2,25 @@
 /**
  * Single Product Image
  *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/product-image.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     2.0.14
+ * @version     2.6.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-global $post, $woocommerce, $product;
+global $post, $product;
 $attachment_count = count( $product->get_gallery_attachment_ids() );
 ?>
 <div class="images">
@@ -20,38 +29,37 @@ $attachment_count = count( $product->get_gallery_attachment_ids() );
 		<?php if ( $attachment_count > 0 ) : ?>
 
 		<div class="col-sm-10 col-sm-push-2">
-		
+
 		<?php else : ?>
 
 		<div class="col-md-12">
 
 		<?php endif; ?>
 
-			<?php
-				if ( has_post_thumbnail() ) {
-
-					$image_title 	= esc_attr( get_the_title( get_post_thumbnail_id() ) );
-					$image_caption 	= get_post( get_post_thumbnail_id() )->post_excerpt;
-					$image_link  	= wp_get_attachment_url( get_post_thumbnail_id() );
-					$image       	= get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array(
-						'title'	=> $image_title,
-						'alt'	=> $image_title
-						) );
-
-					if ( $attachment_count > 0 ) {
-						$gallery = '[product-gallery]';
-					} else {
-						$gallery = '';
-					}
-
-					echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<a href="%s" itemprop="image" class="woocommerce-main-image zoom" title="%s" data-rel="prettyPhoto' . $gallery . '">%s</a>', $image_link, $image_caption, $image ), $post->ID );
-
-				} else {
-
-					echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="%s" />', wc_placeholder_img_src(), __( 'Placeholder', 'woocommerce' ) ), $post->ID );
-
-				}
-			?>
+		<?php
+		if ( has_post_thumbnail() ) {
+			$attachment_count = count( $product->get_gallery_attachment_ids() );
+			$gallery          = $attachment_count > 0 ? '[product-gallery]' : '';
+			$props            = wc_get_product_attachment_props( get_post_thumbnail_id(), $post );
+			$image            = get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array(
+				'title'	 => $props['title'],
+				'alt'    => $props['alt'],
+			) );
+			echo apply_filters(
+				'woocommerce_single_product_image_html',
+				sprintf(
+					'<a href="%s" itemprop="image" class="woocommerce-main-image zoom" title="%s" data-rel="prettyPhoto%s">%s</a>',
+					esc_url( $props['url'] ),
+					esc_attr( $props['caption'] ),
+					$gallery,
+					$image
+				),
+				$post->ID
+			);
+		} else {
+			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="%s" />', wc_placeholder_img_src(), __( 'Placeholder', 'woocommerce' ) ), $post->ID );
+		}
+		?>
 
 		</div>
 
