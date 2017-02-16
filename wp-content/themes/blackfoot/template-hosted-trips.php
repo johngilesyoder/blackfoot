47613bc33d663ca3815a03a9b55849ebac8a4f62
John Yoder <johngilesyoder@gmail.com>
2017-02-15 22:47:40 -0700

    Working on hosted trips

--- wp-content/themes/blackfoot/template-hosted-trips.php
+++ wp-content/themes/blackfoot/template-hosted-trips.php
@@ -9,7 +9,17 @@
 
           <div class="page-summary">
 
-            <?php the_content(); ?>
+            <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+  						<?php the_content(); ?>
+
+  					<?php endwhile; ?>
+
+  					<?php else: ?>
+
+  						<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+  					<?php endif; ?>
 
           </div>
 
