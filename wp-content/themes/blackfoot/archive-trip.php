2750c25309bb203c5de81f75ab515f7493599cda
John Yoder <johngilesyoder@gmail.com>
2017-02-15 22:57:57 -0700

    Working on hosted trips

--- wp-content/themes/blackfoot/archive-trip.php
+++ wp-content/themes/blackfoot/archive-trip.php
@@ -6,10 +6,10 @@
         <div class="col-md-10 col-md-offset-1">
           <!-- Page Title -->
           <h1 class="page-title"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-fly.svg"> Guided Fly Fishing Trips</h1>
-          
+
           <div class="page-summary">
 
-            <?php 
+            <?php
               $post_type = get_post_type_object( get_post_type($post) );
               echo $post_type->description ;
             ?>
@@ -23,6 +23,24 @@
       <div class="container">
 
        <?php get_template_part('loop-trip'); ?>
+
+       <!-- Hosted Trip Block -->
+       <div class="trip">
+         <div class="trip-left">
+           <div class="trip-photo" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>');"></div>
+         </div>
+         <div class="trip-right">
+           <div class="trip-content">
+             <h2 class="trip-title">Hosted Fly Fishing Trips</h2>
+             <p class="trip-summary">While 99% of our business is done booking trips for private parties, on occasion we will host trips to our exclusive lodges as well as new and exciting destinations.</p>
+             <!-- <span class="price">
+
+             </span> -->
+             <a href="/hosted-fly-fishing-trips/" class="btn btn-primary">View our hosted trips</a>
+           </div>
+         </div>
+       </div>
+
 
       </div>
     </section>
