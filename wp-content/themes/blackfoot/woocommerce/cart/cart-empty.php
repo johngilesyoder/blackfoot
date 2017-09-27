80bc026d9a94d32763fa7ba78309614330faa41b
John Yoder <johngilesyoder@gmail.com>
2017-09-27 16:08:13 -0500

    Updating WC templates

--- wp-content/themes/blackfoot/woocommerce/cart/cart-empty.php
+++ wp-content/themes/blackfoot/woocommerce/cart/cart-empty.php
@@ -2,9 +2,18 @@
 /**
  * Empty cart page
  *
+ * This template can be overridden by copying it to yourtheme/woocommerce/cart/cart-empty.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 2.0.0
+ * @version 3.1.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -13,12 +22,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 wc_print_notices();
 
-?>
+/**
+ * @hooked wc_empty_cart_message - 10
+ */
+do_action( 'woocommerce_cart_is_empty' );
 
+if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
 <div class="empty-cart">
   <h2>Uh oh! Your cart is empty.</h2>
   <p>Take a look at some of the products in our shop to get you started.</p>
   <p class="return-to-shop"><a class="btn button wc-backward" href="/shop/"><?php _e( 'Return To Shop', 'woocommerce' ) ?></a></p>
 </div>
 
-<?php do_action( 'woocommerce_cart_is_empty' ); ?>
+<?php endif; ?>
