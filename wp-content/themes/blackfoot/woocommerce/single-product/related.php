d2324a5542eba06f6b3a1939f256a9557723cb0b
John Yoder <johngilesyoder@gmail.com>
2017-09-27 17:30:35 -0500

    Updating WC templates

--- wp-content/themes/blackfoot/woocommerce/single-product/related.php
+++ wp-content/themes/blackfoot/woocommerce/single-product/related.php
@@ -2,13 +2,22 @@
 /**
  * Related Products
  *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/related.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     1.6.4
+ * @version     3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
 
 global $product, $woocommerce_loop;
@@ -35,7 +44,7 @@ $products = new WP_Query( $args );
 
 $woocommerce_loop['columns'] = $columns;
 
-if ( $products->have_posts() ) : ?>
+if ( $related_products ) : ?>
 
 	<div class="related products">
 
@@ -44,13 +53,19 @@ if ( $products->have_posts() ) : ?>
 
 			<?php woocommerce_product_loop_start(); ?>
 
-				<?php while ( $products->have_posts() ) : $products->the_post(); ?>
+				<?php foreach ( $related_products as $related_product ) : ?>
 
-					<?php wc_get_template_part( 'content', 'product' ); ?>
+					<?php
+					 	$post_object = get_post( $related_product->get_id() );
 
-				<?php endwhile; // end of the loop. ?>
+						setup_postdata( $GLOBALS['post'] =& $post_object );
+
+						wc_get_template_part( 'content', 'product' ); ?>
+
+				<?php endforeach; ?>
 
 			<?php woocommerce_product_loop_end(); ?>
+
 		</div>
 
 	</div>
