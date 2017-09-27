24ddaa604797730f3aef5c0860dc7902b839ccfa
John Yoder <johngilesyoder@gmail.com>
2017-09-27 16:22:24 -0500

    Updating WC templates

--- wp-content/themes/blackfoot/woocommerce/content-product.php
+++ wp-content/themes/blackfoot/woocommerce/content-product.php
@@ -13,7 +13,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 2.6.1
+ * @version 3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -27,7 +27,6 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	return;
 }
 ?>
-
 <li <?php $classes[] = 'product-tile'; post_class( $classes ); ?>>
 	<?php
 	/**
